5c488a192ead5df37b17cd94c66cdb52a8e5379f
deBubz <bao.es.hoang2@gmail.com>
2022-11-26 23:36:55 +1100

    part 3 creating components

---
+++ tutorials/tour_of_heroes/3_create_feature_component.md
@@ -0,0 +1,22 @@
+# Create feature component
+
+So far everything is living within `HeroesComponent`
+- displaying the list
+- viewing the detail
+
+For larger projects, this would be hard to maintain.
+
+> This section splits up large components to smaller specified subcomponents.
+> This will ends up with
+> 1. `HeroDetailComponent` present the details of a selected hero
+> 2. `HeroesComponent` presenting the list
+
+## Make the component
+
+## Show the componenyt
+
+## What changed
+
+---
+
+## Summary
--- tutorials/tour_of_heroes/tour_heroes_app/src/app/heroes/heroes.component.ts
+++ tutorials/tour_of_heroes/tour_heroes_app/src/app/heroes/heroes.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../interface/hero.interface'
 
+const HEROES: Hero[] = [
+  { id: 1, name: "Windstorm" },
+  { id: 2, name: "HomeLander" },
+  { id: 3, name: "The Deep" },
+  { id: 4, name: "The Noir" },
+  { id: 5, name: "Huey" },
+]
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[] = HEROES;
+  selectedHero? : Hero;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log("aa");
   }
 
 }
