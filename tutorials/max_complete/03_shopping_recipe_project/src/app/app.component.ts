d72494d438bcbfc25b65d8aae9c85d4fbb362851
deBubz <bao.es.hoang2@gmail.com>
2023-01-25 23:22:59 +1100

    06 - databinding nav my version

--- tutorials/max_complete/03_shopping_recipe_project/src/app/app.component.ts
+++ tutorials/max_complete/03_shopping_recipe_project/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '03_shopping_recipe_project';
+
+  activeView: { recipe: boolean, shopping: boolean} = {
+    recipe: true,
+    shopping: false
+  }
+
+  onNavEmitted(e: {recipe: boolean, shopping: boolean}) {
+    this.activeView = e;
+  }
+
 }
--- tutorials/max_complete/03_shopping_recipe_project/src/app/header/header.component.ts
+++ tutorials/max_complete/03_shopping_recipe_project/src/app/header/header.component.ts
@@ -1,9 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
+
+// class NavObject {
+//   constructor()
+// }
+
 export class HeaderComponent {
+  @Output() onNavChange = new EventEmitter<{recipe: boolean, shopping: boolean}>()
+
   constructor() {}
+
+  navRecipe() {
+    console.log("recipe")
+    this.onNavChange.emit({
+      recipe: true,
+      shopping: false,
+    })
+  }
+
+  navShopping() {
+    console.log("Shop")
+    this.onNavChange.emit({
+      recipe: false,
+      shopping: true,
+    })
+
+  }
 }
