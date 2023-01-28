430b1fc05b5fe5c5eac303bac33b386fba260a20
deBubz <bao.es.hoang2@gmail.com>
2023-01-29 05:00:00 +1100

    repo sync

---
+++ tutorials/ng-book/angular-hello-world/src/app/user-list/user-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent{
+  names : string[] = [
+    "egg", "cheeese", "milk", "apple"
+  ];
+
+
+
+}
